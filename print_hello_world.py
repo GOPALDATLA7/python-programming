aadd093007186e4fa4c159d290181d506fa4cf9c
GOPALDATLA7 <115442333+GOPALDATLA7@users.noreply.github.com>
2024-09-28 11:13:33 +0000

    python basics

--- print_hello_world.py
+++ print_hello_world.py
@@ -1,13 +1,13 @@
 #Printing hello world in python
 print("Hello World")
 
-output:
-Hello World
+#output:
+#Hello World
 
 
 #or another way of printing
 A = "Hello World";
 print(A)
 
-output:
-Hello World
+#output:
+#Hello World
